75a5afcf06af59ede9d429cfd8c934dba09fd517
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2024-03-06 21:18:50 +0300

    code correction

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,10 @@
 function signUpUser(firstName, lastName) {
-    return Promise
-    .then(() => {
-        console.log(`Promise {{firstName: ${firstName}, lastName: ${lastName}}}`);
-    });
+    return Promise.resolve(
+        {
+            firstName,
+            lastName
+        },
+    );
 }
+// Export
 export default signUpUser;
