21dc42146531a1b958d39e6471d354d6675008b2
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2024-03-06 20:45:38 +0300

    code updates

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,7 @@
 function handleResponseFromAPI(promise) {
     // Promise Handling
-    promise.then(() => {
+    return promise
+    .then(() => {
         console.log('Got a response from the API');
         return {status: 200, body: 'success'};
     })
