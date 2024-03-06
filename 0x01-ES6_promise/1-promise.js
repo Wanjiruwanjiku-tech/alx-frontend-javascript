eb1591fa8cd8942e42f42250c6cd9b54724a8b35
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2024-03-06 20:13:33 +0300

    Code updates

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,18 +1,14 @@
 function getFullResponseFromAPI(success) {
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (success){
-                resolve({status: 200, body: 'Success'});
-            }
-            else {
-                reject('The fake API is not working currently');
-            }
-        }, 1200)
+        
+        if (success){
+            resolve({status: 200, body: 'Success'});
+        }
+        else {
+            reject(new Error('The fake API is not working currently'));
+        }
     });
     
 }
-getFullResponseFromAPI().then(value => console.log(value))
-                        .catch(error => console.error(error))
-
 // export
 export default getFullResponseFromAPI;
