b0261362c684ead3451485df568d1ddb04fa4446
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2024-03-06 11:51:01 +0300

    Task One - Resolve and Reject

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (success){
+                resolve({status: 200, body: 'Success'});
+            }
+            else {
+                reject('The fake API is not working currently');
+            }
+        }, 1200)
+    });
+    
+}
+getFullResponseFromAPI().then(value => console.log(value))
+                        .catch(error => console.error(error))
