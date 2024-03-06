5617576367537d226157a578e12694c1101d3e34
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2024-03-06 20:40:58 +0300

    code update

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,3 @@
-// The function takes a promise as an arg and appends 3 event handlers to it
-// If it resolves returns an obj with info
-// If it rejects it returns an empty error obj
-// For every resolution (whether the promise resolves or rejects),
-// it should log the message "Got a response from the API" to the console.
 function handleResponseFromAPI(promise) {
     // Promise Handling
     promise.then(() => {
