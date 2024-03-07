8df3f916432e26770ec7385b72a74edbc8e82b00
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2024-03-07 05:33:16 +0300

    Async and Await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,22 @@
+import { uploadPhoto, createUser } from './utils';
+
+async function asyncUploadUser() {
+    let response = {};
+    try {
+        const myUser = await createUser();
+        const myPhoto = await uploadPhoto();
+        
+        response = {
+            myPhoto,
+            myUser
+        };
+
+    } catch (error) {
+        response = {
+            photo: null,
+            user: null
+        };
+    }
+    return response;
+
+}
