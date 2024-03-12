9fef10ae2de629ea80a8ca01cf6385c66a563d55
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2024-03-12 15:01:34 +0300

    Create an array

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,10 @@
-// A Function that returns an array of object
 function getListStudents() {
+    // A Function that returns an array of object
     const studentList = [];
     studentList.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' })
 
-    //return studentList;
+    return studentList;
 }
 export default getListStudents;
