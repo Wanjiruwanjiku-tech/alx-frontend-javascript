c09df27c759e64b4dfbde29f6721fdc5cee444c1
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2024-03-13 16:55:02 +0300

    Tasks two, three and four

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -9,6 +9,40 @@ interface Teacher {
     //Use an index signature to allow any other attr to be added dynamically
     [key: string]: any;
 }
+//Task_Two
+// Create an interface that extends Teacher
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+// Task_Three
+// Write a function that prints teachers
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+const printTeacher: printTeacherFunction =  (firstName: string, lastName: string): string => `${firstName.charAt(0)}. ${lastName}`;
+//Task Four
+// Write a Class described through an interface
+interface newClassInterface {
+    workOnHomework(): string,
+    displayName(): string
+}
+class StudentClass implements newClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    displayName(): string {
+        return this.firstName;
+    }
+    workOnHomework(): string {
+        return "Currently working";
+    }
+}
 // Example Usage
 const teacher3: Teacher = {
     firstName: 'John',
