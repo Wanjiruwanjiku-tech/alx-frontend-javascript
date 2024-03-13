17fb970f0566595cec85f137f0eb16f72a2dad0c
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2024-03-13 04:20:13 +0300

    code updates

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,12 @@
 // returns an updated map for all items with initial quantity at 1
 
-export default function updateUniqueItems(myMap) {
-    if (!(myMap instanceof Map)){
+export default function updateUniqueItems(map) {
+    if (!(map instanceof Map)){
         throw new Error('Cannot process');
     } else {
-        myMap.forEach((value, key) => {
+        map.forEach((value, key) => {
             if (value === 1) {
-              myMap.set(key, 100);
+              map.set(key, 100);
             }
           });
     }
